e6004117834b7aa92ae9c2ee52f966e5a2b898ee
pgattaboy <53262785+pgattaboy@users.noreply.github.com>
2022-08-24 18:18:03 +0600

    Time: 8 ms (88.56%), Space: 12.9 MB (38.79%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -5,11 +5,8 @@ public:
         len=size(nums);
         vector<int> ans;
         for(i=0;i<len;i++){
-            ans.push_back(nums[i]);
+            nums.push_back(nums[i]);
         }
-        for(i=0;i<len;i++){
-            ans.push_back(nums[i]);
-        }
-        return ans;
+        return nums;
     }
 };
