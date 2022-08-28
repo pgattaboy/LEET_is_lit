4b0b013ef3dcd5c2f258e66a8e6acbb0f8fc4bfd
pgattaboy <53262785+pgattaboy@users.noreply.github.com>
2022-08-29 01:22:33 +0600

    Time: 24 ms (14.63%), Space: 15.2 MB (77.10%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
         int i,n=s.size();
-        string str;
         for(i=0;i<n;i++){
             while(i!=indices[i]){
                 swap(s[i],s[indices[i]]);
