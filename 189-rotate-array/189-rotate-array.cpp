80e16f45a18330398ea5c93db0f99af87150400a
pgattaboy <53262785+pgattaboy@users.noreply.github.com>
2022-09-02 00:44:59 +0600

    Time: 37 ms (80.89%), Space: 25.1 MB (57.12%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n=nums.size(),i;
-        vector<int> res(n);
-        for(i=0;i<n;i++){
-            res[(i+k)%n]=nums[i];
-        }
-        for(i=0;i<n;i++)
-           nums[i]=res[i];
+           //reverse(v.begin(),v.end()) reverses the whole vector
+        //we can specify from where our function can start reversing...except the .end() everyother ending value is exclusive.
+        int n=nums.size();
+        k%=n;
+        reverse(nums.begin(),nums.end());
+      
+        reverse(nums.begin(),nums.begin()+k);
+       
+        reverse(nums.begin()+k,nums.end());
+        
     }
 };
