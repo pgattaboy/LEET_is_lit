0362d05a8afe2e5d14575ba221edb3eba27695fc
pgattaboy <53262785+pgattaboy@users.noreply.github.com>
2022-09-05 08:45:47 +0600

    Time: 13 ms (25.80%), Space: 10.2 MB (42.55%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
+        if(nums[0]<0)nums[0]=-1;
+        else if(nums[0]>0)nums[0]=1;
         for(int i=1;i<nums.size();i++){
             if(nums[i]<0)
                 nums[0]*=-1;
             else if(nums[i]==0){
-               return signFunc(0);
+               return 0;
             }
             else
                 nums[0]*=1;
         }
-        return signFunc(nums[0]);
+        return nums[0];
     }
-    int signFunc(int x){
-    if(x<0)
-        return -1;
-    else if(x>0)
-        return 1;
-    else
-        return 0;
-}
 };
