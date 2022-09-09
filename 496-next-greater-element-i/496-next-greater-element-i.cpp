6f18ed3fc6c0839e6511bb1c82b6c0cfb2391b70
pgattaboy <53262785+pgattaboy@users.noreply.github.com>
2022-09-09 23:53:37 +0600

    Time: 15 ms (32.79%), Space: 9 MB (32.38%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>n_greater(nums1.size());
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    n_greater[i]=j;
-                    break;
-                }
-            }
+        map<int,int> mp;
+        for(int i=0;i<nums2.size();i++){
+            mp[nums2[i]]=i;
         }
         for(int i=0;i<nums1.size();i++){
-            int j=n_greater[i]+1;
-            n_greater[i]=-1;
+            int j=mp[nums1[i]]+1;
+          mp[nums1[i]]=-1;
             for(;j<nums2.size();j++){
                 if(nums1[i]<nums2[j]){
-                    n_greater[i]=nums2[j];
+                   mp[nums1[i]]=nums2[j];
                     break;
                 }
             }
+            nums1[i]=mp[nums1[i]];
         }
-        return n_greater;
+        return nums1;
     }
 };
